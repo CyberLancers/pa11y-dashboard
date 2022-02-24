d4371cc569044e409a3b69e0070d578b965d0e81
Jeff Love <jlove@cyberlancers.com>
2022-02-23 20:01:32 -0700

    removing BOM

--- route/wallboard.js
+++ route/wallboard.js
@@ -1,4 +1,4 @@
-﻿// This file is part of Pa11y Dashboard.
+// This file is part of Pa11y Dashboard.
 //
 // Pa11y Dashboard is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
